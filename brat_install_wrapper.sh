1c485b2a79de5dd10526bde5fcb6a856778331d0
Elvis MBONING <levismboning@ntealan.org>
2023-09-20 03:33:05 +0200

    git add data product

--- brat_install_wrapper.sh
+++ brat_install_wrapper.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-cd /var/www/brat/brat-v1.3_Crunchy_Frog && /var/www/brat/brat-v1.3_Crunchy_Frog/install.sh <<EOD 
+echo "BRAT_USERNAME: $BRAT_USERNAME"
+echo "BRAT_PASSWORD: $BRAT_PASSWORD"
+echo "BRAT_EMAIL: $BRAT_EMAIL"
+
+cd /var/www/brat && /var/www/brat/install.sh <<EOD 
 $BRAT_USERNAME 
 $BRAT_PASSWORD 
 $BRAT_EMAIL
 EOD
 
+echo "patch the user config with more users"
+
 chown -R www-data:www-data /bratdata
 
 # patch the user config with more users
-python /var/www/brat/brat-v1.3_Crunchy_Frog/user_patch.py
+python2 /var/www/brat/user_patch.py
 
 echo "Install complete. You can log in as: $BRAT_USERNAME"
 
